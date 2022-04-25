6a76f6ac8c57b0f5d840c0d4d70b0021cdd4836b
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-04-25 17:44:12 -0400

    using action generator to return objects

--- redux-app/src/playground/redux-101.js
+++ redux-app/src/playground/redux-101.js
@@ -1,15 +1,46 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }, action) => {
-  const incrementBy =
-    typeof action.incrementBy === "number" ? action.incrementBy : 1;
+//action generator
 
+const incrementCount = ({ incrementBy = 1 } = {}) => {
+  return {
+    type: "INCREMENT",
+    incrementBy,
+  };
+};
+
+const decrementCount = ({ decrementBy = 1 } = {}) => {
+  return {
+    type: "DECREMENT",
+    decrementBy,
+  };
+};
+
+const setCount = ({ count }) => {
+  return {
+    type: "SET",
+    count,
+  };
+};
+
+const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: state.count + incrementBy };
+      //   const incrementBy =
+      //     typeof action.incrementBy === "number" ? action.incrementBy : 1;
+
+      return { count: state.count + action.incrementBy };
 
     case "DECREMENT":
-      return { count: state.count - 1 };
+      //   const decrementBy =
+      //     typeof action.decrementBy === "number" ? action.decrementBy : 1;
+
+      return { count: state.count - action.decrementBy };
+
+    case "SET":
+      return {
+        count: action.count,
+      };
 
     case "RESET":
       return { count: 0 };
@@ -23,21 +54,30 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5,
-});
+// store.dispatch({
+//   type: "INCREMENT",
+//   incrementBy: 5,
+// });
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: "INCREMENT",
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+// store.dispatch({
+//   type: "INCREMENT",
+// });
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 4 }));
+
+store.dispatch(
+  setCount({
+    count: 110,
+  })
+);
 
 unsubscribe();
 
 store.dispatch({
   type: "RESET",
-});
-
-store.dispatch({
-  type: "DECREMENT",
 });
