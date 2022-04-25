c444c84390059d84f91aea64393cb6025f3f7be7
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-04-25 11:36:40 -0400

    redux practice

--- redux-app/src/playground/redux-101.js
+++ redux-app/src/playground/redux-101.js
@@ -1,5 +1,43 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
+const store = createStore((state = { count: 0 }, action) => {
+  const incrementBy =
+    typeof action.incrementBy === "number" ? action.incrementBy : 1;
 
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + incrementBy };
 
-console.log("test 101")
+    case "DECREMENT":
+      return { count: state.count - 1 };
+
+    case "RESET":
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5,
+});
+
+store.dispatch({
+  type: "INCREMENT",
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "RESET",
+});
+
+store.dispatch({
+  type: "DECREMENT",
+});
